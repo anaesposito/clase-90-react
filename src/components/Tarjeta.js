4e029548357b9ad2c1e717b4d7543fe97c0bc974
anaesposito <anaclaraesposito@gmail.com>
2021-03-21 17:30:14 -0300

    homework accomplished

---
+++ src/components/Tarjeta.js
@@ -0,0 +1,29 @@
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
+import "./Tarjeta.scss";
+import StarIcon from "@material-ui/icons/Star";
+const Tarjeta = ({ title, type, price, rating, img, isAvailable, onSale }) => {
+  return (
+    <div>
+      <div>
+        <h1>{title}</h1>
+        <p>{isAvailable ? "Disponible" : "No Disponible"}</p>
+        <p>{onSale ? "EN OFERTA" : ""}</p>
+        <p>Tipo: {type}</p>
+        <p>${price}</p>
+        <p>
+          {rating === 4 ? (
+            <span>
+              <StarIcon /> <StarIcon /> <StarIcon />
+              <StarIcon />
+            </span>
+          ) : (
+            ""
+          )}
+        </p>
+      </div>
+      <img src={img} />
+    </div>
+  );
+};
+
+export default Tarjeta;
